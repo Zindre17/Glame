0a71a69488a2b90e27f0157b525dd0c00570651f
Sindre Aarstrand <zindre17@gmail.com>
2022-06-19 20:32:51 +0200

    Added vertex array abstraction

---
+++ VertexArray.cs
@@ -0,0 +1,81 @@
+using Silk.NET.OpenGL;
+using static Glame.OpenGlContext;
+
+namespace Glame;
+
+public class VertexArray : IDisposable
+{
+    private readonly uint id;
+
+    public VertexArray()
+    {
+        id = Gl.GenVertexArray();
+        Bind();
+    }
+
+    unsafe public void AddBuffer<T>(VertexBuffer<T> buffer, VertexLayout<T> layout)
+        where T : unmanaged
+    {
+        Bind();
+        buffer.Bind();
+
+        var stride = layout.Stride;
+        var glType = GetGlType(typeof(T));
+
+        var index = 0u;
+        var offset = 0u;
+        foreach (var attribute in layout.Attributes)
+        {
+            Gl.EnableVertexAttribArray(index);
+            Gl.VertexAttribPointer(
+                index++,
+                attribute.Count,
+                glType,
+                attribute.Normalized,
+                stride,
+                (void*)offset
+            );
+
+            offset += (uint)(attribute.Count * sizeof(T));
+        }
+    }
+
+    public void Bind()
+    {
+        Gl.BindVertexArray(id);
+    }
+
+    public void Unbind()
+    {
+        Gl.BindVertexArray(0);
+    }
+
+    public void Dispose()
+    {
+        Gl.DeleteVertexArray(id);
+    }
+
+    private static GLEnum GetGlType(Type type)
+    {
+        if (type == typeof(float))
+        {
+            return GLEnum.Float;
+        }
+        else if (type == typeof(int))
+        {
+            return GLEnum.Int;
+        }
+        else if (type == typeof(uint))
+        {
+            return GLEnum.UnsignedInt;
+        }
+        else if (type == typeof(byte))
+        {
+            return GLEnum.Byte;
+        }
+        else
+        {
+            throw new ArgumentException("Unsupported type");
+        }
+    }
+}
